559b8f2bf765f5717751d924cf91f3eb2dd40ceb
lly <849851251@qq.com>
2018-11-16 10:23:28 +0800

    test

---
+++ app/src/main/java/com/lly/socketgame/house/CreateHouseActivity.java
@@ -0,0 +1,13 @@
+package com.lly.socketgame.house;
+
+/**
+ * CreateHouseActivity[v 1.0.0]
+ * classes:com.lly.socketgame.house.CreateHouseActivity
+ *
+ * @author lileiyi
+ * @date 2018/11/16
+ * @time 10:16
+ * @description
+ */
+public class CreateHouseActivity {
+}
